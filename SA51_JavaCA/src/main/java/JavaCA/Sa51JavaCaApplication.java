831ab56efc88e111649802aefbe4e90f898d58ec
DeJustin <makebelief@icloud.com>
2020-12-11 16:51:17 +0800

    A new project is born!

---
+++ SA51_JavaCA/src/main/java/JavaCA/Sa51JavaCaApplication.java
@@ -0,0 +1,13 @@
+package JavaCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa51JavaCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa51JavaCaApplication.class, args);
+	}
+
+}
