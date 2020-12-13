c4fbcdd92ebfd4bd8f53ec4eb71c02f15909c843
DeJustin <makebelief@icloud.com>
2020-12-13 16:43:43 +0800

    adding a comment

--- SA51_JavaCA/src/main/java/JavaCA/Sa51JavaCaApplication.java
+++ SA51_JavaCA/src/main/java/JavaCA/Sa51JavaCaApplication.java
@@ -8,6 +8,7 @@ public class Sa51JavaCaApplication {
 
 	public static void main(String[] args) {
 		// Hi it's jon
+		// Hi its justin too
 		SpringApplication.run(Sa51JavaCaApplication.class, args);
 	}
 
