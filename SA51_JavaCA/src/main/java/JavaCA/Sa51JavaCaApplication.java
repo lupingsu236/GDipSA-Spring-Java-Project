d57b6c75a50d5e4b93e51ea5b9d966cf05ccb3b9
liauhyj <72335756+liauhyj@users.noreply.github.com>
2020-12-13 15:56:03 +0800

    First commit Jon branch

--- SA51_JavaCA/src/main/java/JavaCA/Sa51JavaCaApplication.java
+++ SA51_JavaCA/src/main/java/JavaCA/Sa51JavaCaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Sa51JavaCaApplication {
 
 	public static void main(String[] args) {
+		// Hi it's jon
 		SpringApplication.run(Sa51JavaCaApplication.class, args);
 	}
 
