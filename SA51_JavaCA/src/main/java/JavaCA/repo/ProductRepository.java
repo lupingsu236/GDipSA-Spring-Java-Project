25d246d10b4d084c9f4b78eb788c06c7f99bf912
lupingsu236 <72119175+lupingsu236@users.noreply.github.com>
2020-12-14 16:29:56 +0800

    Added view product listing + product detail use cases

--- SA51_JavaCA/src/main/java/JavaCA/repo/ProductRepository.java
+++ SA51_JavaCA/src/main/java/JavaCA/repo/ProductRepository.java
@@ -1,10 +1,18 @@
 package JavaCA.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import JavaCA.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> 
 {
-
+	@Query("SELECT p FROM Product p JOIN p.brand JOIN p.supplier")
+	public List<Product> findAllProducts();
+	
+	@Query("SELECT p FROM Product p JOIN p.brand JOIN p.supplier WHERE p.id = :pid")
+	public Product findProduct(@Param("pid") long id);
 }
