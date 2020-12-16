ab04f9f014585e39879b6d2073b38d229f3cba85
liauhyj <72335756+liauhyj@users.noreply.github.com>
2020-12-16 20:56:49 +0800

    Latest push

--- SA51_JavaCA/src/main/java/JavaCA/service/UserInterface.java
+++ SA51_JavaCA/src/main/java/JavaCA/service/UserInterface.java
@@ -1,4 +1,4 @@
-package JavaCA.interfaceimplementation;
+package JavaCA.service;
 
 import java.util.List;
 
---
+++ SA51_JavaCA/src/test/java/JavaCA/Sa51JavaCaApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa51JavaCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
