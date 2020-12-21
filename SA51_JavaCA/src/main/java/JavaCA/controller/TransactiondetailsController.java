f3dc6cebc0bf75099496c7ea75e2623dc31d5130
DeJustin <makebelief@icloud.com>
2020-12-21 12:30:37 +0800

    trying @valid and bindingresult

--- SA51_JavaCA/src/main/java/JavaCA/controller/TransactiondetailsController.java
+++ SA51_JavaCA/src/main/java/JavaCA/controller/TransactiondetailsController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -76,8 +77,8 @@ public class TransactiondetailsController {
 	}
 	
 	@PostMapping("/save/{tid}")
-	public String saveTransactionDetails(@PathVariable("tid") int tid, RedirectAttributes redirectModel,
-			@ModelAttribute("td") TransactionDetail td, HttpSession session) {
+	public String saveTransactionDetails(@PathVariable("tid") int tid, @Valid @ModelAttribute("td") TransactionDetail td,
+				BindingResult bindingResult, RedirectAttributes redirectModel, HttpSession session) {
 		Transaction t = transactionService.findTransactionById(tid);
 		td.setTransaction(t);
 		Product p = productService.findProduct(td.getProduct().getId());
--- SA51_JavaCA/src/main/java/JavaCA/model/TransactionDetail.java
+++ SA51_JavaCA/src/main/java/JavaCA/model/TransactionDetail.java
@@ -8,12 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.validator.constraints.Range;
+
 @Entity
 public class TransactionDetail 
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	@Range(min = 1, message = "Minimum quantity change is 1")
 	private int quantityChange;
 	private Date date;
 	private TransactionType transactionType;
