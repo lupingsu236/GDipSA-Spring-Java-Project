a3fb6a3b488f0f92d5ea7a38a2e2c495a0b2a2e5
DeJustin <makebelief@icloud.com>
2020-12-22 13:50:51 +0800

    validation folder

---
+++ SA51_JavaCA/src/main/java/JavaCA/validation/CarplateValidator.java
@@ -0,0 +1,20 @@
+package JavaCA.validation;
+//pls push
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CarplateValidator implements  ConstraintValidator<CarplateContraint, String> {
+	
+	@Override
+    public void initialize(CarplateContraint carplate) {
+    }
+	
+	@Override
+    public boolean isValid(String carplate, ConstraintValidatorContext cxt) {
+		
+		if (carplate == null) {return true;}
+		if (carplate.length() == 0) {return true;}
+		if (carplate.substring(0, 1).equalsIgnoreCase("S") || carplate.substring(0, 1).equalsIgnoreCase("E")) {return true;}
+        return false;
+    }
+}
